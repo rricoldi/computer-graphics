7add4060d8b52782d903f87b490f38f38c9233e6
Renan Ricoldi <renan.ricoldi.frois@gmail.com>
2021-09-24 11:42:46 -0300

    feat: implemented filters

--- discreet-cos-transform/main.py
+++ discreet-cos-transform/main.py
@@ -7,18 +7,18 @@ image = cv.imread(sys.argv[1])
 
 b, g, r = cv.split(image)
 
-# histogram = utils.get_histogram(b)
-# dct_histogram = utils.get_dct_by_histogram(histogram, 256)
-
-# utils.print_array_into_file('result.txt', histogram)
-# utils.print_array_into_file('result-dct.txt', dct_histogram)
-
-# utils.show_histogram(histogram, 'Histogram')
-# utils.show_histogram(dct_histogram, 'DCT')
 utils.print_matrix_into_file('image.txt', b)
 
 dct_matrix = utils.get_bi_dimensional_dct_by_image(b)
-dct_image = utils.get_image_by_bi_dimensional_dct(dct_matrix)
-utils.print_matrix_into_file('result.txt', dct_image)
-cv.imwrite('blue.jpg', b)
-cv.imwrite('result.jpg', dct_image)
+image_filter = utils.get_filtered_matrix(utils.fpbi, dct_matrix, 18)
+filtered_frequency = utils.get_image_filtered(dct_matrix, image_filter)
+fpbi_image = utils.get_image_by_bi_dimensional_dct(filtered_frequency)
+
+image_filter = utils.get_filtered_matrix(utils.fpai, dct_matrix, 18)
+filtered_frequency = utils.get_image_filtered(dct_matrix, image_filter)
+fpai_image = utils.get_image_by_bi_dimensional_dct(filtered_frequency)
+
+
+cv.imwrite('blue-band.jpg', b)
+cv.imwrite('filtered-with-fpbi-image.jpg', fpbi_image)
+cv.imwrite('filtered-with-fpai-image.jpg', fpai_image)
--- discreet-cos-transform/utils.py
+++ discreet-cos-transform/utils.py
@@ -7,19 +7,6 @@ import os
 def get_matrix_of_zeros(width, height):
   return np.zeros((width, height))
 
-# def get_histogram(image):
-#   height, width = image.shape
-
-#   histogram = get_array_of_zeros(256)
-
-#   for column in range(height):
-#     for row in range(width):
-#       intensity = image[column, row]
-
-#       histogram[intensity] += 1
-
-#   return histogram
-
 def get_alpha(u, n):
   if u == 0:
     return math.sqrt(1/n)
@@ -30,39 +17,6 @@ def get_cos(x, u, n):
   z = (2*x + 1)*u*math.pi
   return math.cos(z/(2*n))
 
-# def get_dct_by_u(u, n, histogram):
-#   alpha = get_alpha(u,  n)
-#   aux_sum = 0
-#   for x in range(n-1):
-#     aux_sum += (histogram[x] * get_cos(x, u, n))
-  
-#   return alpha * aux_sum
-
-# def get_dct_by_histogram(histogram, n):
-#   dct_histogram = get_array_of_zeros(n)
-#   for u in range(n-1):
-#     dct_histogram[u] = get_dct_by_u(u, n, histogram)
-
-#   return dct_histogram
-
-# def show_histogram(histogram, name):
-#   plot.title(name)
-#   plot.plot(histogram)
-#   plot.xlabel('X')
-#   plot.ylabel('Y')
-#   plot.show()
-
-# def print_array_into_file(textFilePath, array):
-#   if os.path.exists(textFilePath):
-#     os.remove(textFilePath)
-
-#   textFile = open(textFilePath, 'x')
-#   counter = 0
-#   for row in range(len(array)):
-#     counter += array[row]
-#     textFile.write('{} - {} '.format(row, array[row]))
-#     textFile.write('\n')
-#   textFile.write('{} px'.format(counter))
 
 def get_normalized_value(number: float):
   return number/255
@@ -80,11 +34,13 @@ def get_dct_by_u_v(u, v, n, m, image):
 
 def get_bi_dimensional_dct_by_image(image):
   height, width = image.shape
-  dct_image = get_matrix_of_zeros(width, height)
-
-  for u in range(width):
-    for v in range(height):
-      dct_image[u][v]= get_dct_by_u_v(u, v, width, height, image)
+  dct_image = get_matrix_of_zeros(height, width)
+  counter = 0
+  for u in range(height):
+    print('{:.2f}%'.format((counter*100)/(height*width)))
+    for v in range(width):
+      counter += 1
+      dct_image[u][v]= get_dct_by_u_v(u, v, height, width, image)
 
   return dct_image
 
@@ -103,13 +59,44 @@ def get_pixel_by_u_v(x, y, n, m, image):
 def get_image_by_bi_dimensional_dct(dct_matrix):
   height = len(dct_matrix)
   width = len(dct_matrix[0])
-  image = get_matrix_of_zeros(width, height)
+  image = get_matrix_of_zeros(height, width)
 
-  for x in range(width):
-    for y in range(height):
-      image[x][y]= get_pixel_by_u_v(x, y, width, height, dct_matrix)
+  for x in range(height):
+    for y in range(width):
+      image[x][y]= get_pixel_by_u_v(x, y, height, width, dct_matrix)
 
   return image
+
+def get_distance_from_init(x, y):
+  return math.sqrt((math.pow(x, 2) + math.pow(y, 2)))
+
+def fpbi(x, y, d0):
+  if(get_distance_from_init(x, y) <= d0):
+    return 1
+  else:
+    return 0
+
+def fpai(x, y, d0):
+  if(get_distance_from_init(x, y) <= d0):
+    return 0
+  else:
+    return 1
+
+
+def get_filtered_matrix(filter_function, image, d0):
+  height = len(image)
+  width = len(image[0])
+  filter = get_matrix_of_zeros(height, width)
+
+  for x in range(height):
+    for y in range(width):
+      filter[x][y] = filter_function(x, y, d0)
+
+  return filter
+
+def get_image_filtered(image, filtered_frequency):
+  return image * filtered_frequency
+
 
 def print_matrix_into_file(textFilePath, matrix):
   if os.path.exists(textFilePath):
